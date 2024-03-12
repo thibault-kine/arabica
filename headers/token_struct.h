69acb69f24aab4161e049261ee9209ea70517eb4
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2024-03-12 15:23:37 +0100

    feat: token table

--- headers/token_struct.h
+++ headers/token_struct.h
@@ -13,12 +13,33 @@ typedef enum t_token_type {
 } TokenType;
 
 /**
+ * @param index The index according to the documentation
  * @param type Type from the TokenType enum
  * @param value Token value as a string
 */
 typedef struct t_token {
+    int index;
     TokenType type; 
     char* value;
 } Token;
+
+const char* TOKENS_STRS[] = {
+    "LOAD_VAL",
+    "READ_VAR",
+    "STORE_VAR",
+    "ADD", "SUB", "MUL", "DIV", "MOD",
+    "JMP", "JMP_IF_ZERO", "JMP_IF_TRUE",
+    "EQ", "NEQ", 
+    "GT", "LT", "GTE", "LTE",
+    "PRINT_VAL",
+    "INPUT_VAL",
+    "HALT",
+    "LOAD_STR",
+    "PRINT_STR", "INPUT_STR", "STR_LEN",
+    "CONCAT",
+    "GET_CHAR",
+    "SET_CHAR",
+    "STR_CMP"
+};
 
 #endif
--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "headers/token_struct.h"
 
 int main(void) {
-    Token myToken;
-    myToken.type = LIT;
-    myToken.value = "HELLO";
+    char* token_str = "STR_CMP";
+    int i = 0;
+    while(TOKENS_STRS[i] != NULL) {
+        if(strcmp(token_str, TOKENS_STRS[i]) == 0) {
+            Token myToken;
+            myToken.index = i + 1;
+            myToken.value = token_str;
+            myToken.type = KWD;
 
-    printf("%s\n", myToken.value);
+            printf("%x : %s\n", myToken.index, myToken.value);
+        }
+        i++;
+    }
 }
